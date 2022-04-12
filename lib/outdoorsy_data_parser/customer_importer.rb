b1960d46c87515b7ca765f3f91995b9b8d3b581d
btmccollum <brad.mccollum@hey.com>
2022-04-12 17:24:49 -0400

    account for units in attr name

--- lib/outdoorsy_data_parser/customer_importer.rb
+++ lib/outdoorsy_data_parser/customer_importer.rb
@@ -15,16 +15,7 @@ class CustomerImporter
   def self.parse_file
     File.open(@file_path) do |file|
       CSV.foreach(file, col_sep: determine_probable_seperator(file.readline)) do |row|
-        customer = Customer.new(first_name: row[0], last_name: row[1], email: row[2])
-        vehicle = Vehicle.new(type: row[3], name: row[4], length_ft: row[5])
-
-        @data << {
-          full_name: customer.full_name,
-          email: customer.email,
-          vehicle_type: vehicle.type,
-          vehicle_name: vehicle.name,
-          vehicle_length_ft: vehicle.length_ft
-        }
+        add_customer_data_from_row(row)
       end
     end
     @data
@@ -32,6 +23,19 @@ class CustomerImporter
 
   def self.determine_probable_seperator(line)
     COMMON_SEPERATORS.group_by { |seperator| line.count(seperator) }.max.flatten[1]
+  end
+
+  def self.add_customer_data_from_row(row)
+    customer = Customer.new(first_name: row[0], last_name: row[1], email: row[2])
+    vehicle = Vehicle.new(type: row[3], name: row[4], length_ft: row[5])
+
+    @data << {
+      full_name: customer.full_name,
+      email: customer.email,
+      vehicle_type: vehicle.type,
+      vehicle_name: vehicle.name,
+      vehicle_length_ft: vehicle.length_ft
+    }
   end
 
   class << self
