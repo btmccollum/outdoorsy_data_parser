da255c2c6ec9d26d0313e979f3b6138a387fb9ab
btmccollum <brad.mccollum@hey.com>
2022-04-12 15:46:38 -0400

    add handling for different delimiters

--- lib/outdoorsy_data_parser/customer_importer.rb
+++ lib/outdoorsy_data_parser/customer_importer.rb
@@ -4,6 +4,8 @@ require 'pry'
 
 # Handle CSV parsing
 class CustomerImporter
+  COMMON_SEPERATORS = ['|', ',', ';', '#', '/t'].freeze
+
   def self.import(file_path:)
     @file_path = file_path
     @data = []
@@ -12,7 +14,7 @@ class CustomerImporter
 
   def self.parse_file
     File.open(@file_path) do |file|
-      CSV.parse(file) do |row|
+      CSV.foreach(file, col_sep: determine_probable_seperator(file.readline)) do |row|
         customer = Customer.new(first_name: row[0], last_name: row[1], email: row[2])
         vehicle = Vehicle.new(type: row[3], name: row[4], length: row[5])
 
@@ -26,5 +28,13 @@ class CustomerImporter
       end
     end
     @data
+  end
+
+  def self.determine_probable_seperator(line)
+    COMMON_SEPERATORS.group_by { |sep| line.count(sep) }.max.flatten[1]
+  end
+
+  class << self
+    private :parse_file, :determine_probable_seperator
   end
 end
--- spec/customer_importer_spec.rb
+++ spec/customer_importer_spec.rb
@@ -5,7 +5,7 @@ require 'outdoorsy_data_parser/customer_importer'
 RSpec.describe CustomerImporter do
   let(:commas_path) { './spec/fixtures/files/commas.txt' }
   let(:pipes_path) { './spec/fixtures/files/pipes.txt' }
-  let(:result) do
+  let(:commas_result) do
     [
       {
         full_name: 'greta thunberg',
@@ -37,13 +37,58 @@ RSpec.describe CustomerImporter do
       }
     ]
   end
+  let(:pipes_result) do
+    [
+      {
+        full_name: 'ansel adams',
+        email: 'a@adams.com',
+        vehicle_type: 'motorboat',
+        vehicle_name: 'rushing water',
+        vehicle_length: '24’'
+      },
+      {
+        full_name: 'steve irwin',
+        email: 'steve@crocodiles.com',
+        vehicle_type: 'rv',
+        vehicle_name: 'g’day for adventure',
+        vehicle_length: '32 ft'
+      },
+      {
+        full_name: 'isatou ceesay',
+        email: 'isatou@recycle.com',
+        vehicle_type: 'campervan',
+        vehicle_name: 'plastic to purses',
+        vehicle_length: '20’'
+      },
+      {
+        full_name: 'naomi uemura',
+        email: 'n.uemura@gmail.com',
+        vehicle_type: 'bicycle',
+        vehicle_name: 'glacier glider',
+        vehicle_length: '5 feet'
+      }
+    ]
+  end
 
   it 'accepts a file with comma separated values' do
     expect(File).to receive(:open).with(commas_path)
     CustomerImporter.import(file_path: commas_path)
   end
 
-  it 'returns an array of hashes with customer data from csv' do
-    expect(CustomerImporter.import(file_path: './spec/fixtures/files/commas.txt')).to eq(result)
+  it 'accepts a file with pipe delimited values' do
+    expect(File).to receive(:open).with(pipes_path)
+    CustomerImporter.import(file_path: pipes_path)
+  end
+
+  it 'returns an array of hashes with customer data from comma separated values' do
+    data = CustomerImporter.import(file_path: commas_path)
+    expect(data).to eq(commas_result)
+    expect(data.length).to eq(4)
+  end
+
+  it 'returns an array of hashes with customer data from pipe delimited file' do
+    data = CustomerImporter.import(file_path: pipes_path)
+    expect(data).to eq(pipes_result)
+    expect(data.length).to eq(4)
   end
 end
