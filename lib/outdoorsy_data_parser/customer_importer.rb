15f6306d7c50e8b6478b48e0190b824a86028645
btmccollum <brad.mccollum@hey.com>
2022-04-12 10:49:57 -0400

    add customer to parser

--- lib/outdoorsy_data_parser/customer_importer.rb
+++ lib/outdoorsy_data_parser/customer_importer.rb
@@ -13,15 +13,14 @@ class CustomerImporter
   def self.parse_file
     File.open(@file_path) do |file|
       CSV.parse(file) do |row|
-        first_name = row[0]
-        last_name = row[1]
+        customer = Customer.new(first_name: row[0], last_name: row[1])
         email = row[2]
         vehicle_type = row[3]
         vehicle_name = row[4]
         vehicle_length = row[5]
 
         @data << {
-          full_name: "#{first_name} #{last_name}",
+          full_name: customer.full_name,
           email: email,
           vehicle_type: vehicle_type,
           vehicle_name: vehicle_name,
